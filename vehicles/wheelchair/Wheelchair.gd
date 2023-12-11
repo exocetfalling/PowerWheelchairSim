faf427cc3563b7b0618f59ad5797b875e2fd32c1
exocetfalling <70193215+exocetfalling@users.noreply.github.com>
2023-12-11 17:26:11 +0800

    Fixed castoring wheels.

--- vehicles/wheelchair/Wheelchair.gd
+++ vehicles/wheelchair/Wheelchair.gd
@@ -16,11 +16,18 @@ var steering_lerp_factor : float = 0.2
 var wheel_speed_rear_left = 0.00
 var wheel_speed_rear_right = 0.00
 
+var turn_radius: float = 0.00
+var wheel_velocity_ratio: float = 0.00
+export var wheelbase_width: float = 0.49
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	DebugOverlay.stats.add_property(self, "input_joystick", "round")
 	DebugOverlay.stats.add_property(self, "angular_velocity_local", "round")
 	DebugOverlay.stats.add_property(self, "steering_angle_target", "round")
+	DebugOverlay.stats.add_property(self, "steering", "round")
+	DebugOverlay.stats.add_property(self, "wheel_velocity_ratio", "")
+	DebugOverlay.stats.add_property(self, "turn_radius", "")
 	pass # Replace with function body.
 
 func interpolate_linear(value_current, value_target, rate, delta_time):
@@ -72,10 +79,28 @@ func _physics_process(delta):
 	# Interpolate steering angles
 	# Convert to rad. for proper use of lerp_angle()
 	# Convert back to deg. for steering
-	steering = rad2deg(lerp_angle(deg2rad(steering), deg2rad(steering_angle_target), steering_lerp_factor))
+#	steering = rad2deg(lerp_angle(deg2rad(steering), deg2rad(steering_angle_target), steering_lerp_factor))
 	
 #	steering = interpolate_linear(steering, steering_angle_target, 60, delta)
+#	steering = rad2deg(atan2(0.391, turn_radius))
 	
+	wheel_speed_rear_left = $WheelRearLeft.get_rpm() * 0.1047
+	wheel_speed_rear_right = $WheelRearRight.get_rpm() * 0.1047
+	
+	if (wheel_speed_rear_left - wheel_speed_rear_right) != 0:
+		wheel_velocity_ratio = \
+			(wheel_speed_rear_left + wheel_speed_rear_right) / \
+			(wheel_speed_rear_left - wheel_speed_rear_right)
+	else:
+		wheel_velocity_ratio = INF
+	
+	turn_radius = -wheel_velocity_ratio * 0.4
+	
+	$WheelFrontLeft.steering = rad2deg(atan2(0.391, (turn_radius + $WheelFrontLeft.translation.x)))
+	$WheelFrontRight.steering = rad2deg(atan2(0.391, (turn_radius + $WheelFrontRight.translation.x)))
+	
+	
+#	$CSGSphere.translation.x = turn_radius
 	
 	pass
 
@@ -85,8 +110,9 @@ func _process(delta):
 	$Model/WheelRearLeft.rotation = $WheelRearLeft.rotation
 	$Model/WheelRearRight.rotation = $WheelRearRight.rotation
 	
-	$Model/CastorLeft.rotation.y = deg2rad(steering)
-	$Model/CastorRight.rotation.y = deg2rad(steering)
+	$Model/CastorLeft.rotation.y = deg2rad($WheelFrontLeft.steering)
+	$Model/CastorRight.rotation.y = deg2rad($WheelFrontRight.steering)
+	
 	
 	$Model/CastorLeft/WheelFrontLeft.rotation.x = $WheelFrontLeft.rotation.x
 	$Model/CastorRight/WheelFrontRight.rotation.x = $WheelFrontRight.rotation.x
