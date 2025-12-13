71b5cd641570a37a55c544346101228d42aa2690
exocetfalling <70193215+exocetfalling@users.noreply.github.com>
2025-12-13 16:51:12 +0800

    Add gyroscope controls and input actions for VR
    Introduced new input actions 'gyro_toggle' and 'cam_align_fwd' in project settings and enabled device sensors. Updated CardboardVR script to handle gyroscope toggling, camera alignment, and input-based rotation, moving input handling to _physics_process for improved responsiveness.

--- uires/cardboard_vr/cardboard_vr.gd
+++ uires/cardboard_vr/cardboard_vr.gd
@@ -2,24 +2,36 @@ class_name CardboardVR extends Node3D
 
 @export_category("Controls")
 @export var use_gyroscope : bool = true
-@export var mouse_sensitivity : float = 0.003
-@export var gyro_factor : float = 0.2
-@export var handle_mouse_capture : bool = true
-@export var input_event_free_mouse : String  = "ui_cancel"
+@export var gyro_factor : float = 1
 
 @export_category("Eyes")
 @export_range(0.001, 0.1) var eye_distance : float = 0.063
 @export_range(0, 5.0) var eye_height : float =  0.8
 @export_range(-360, 360) var eye_convergence_angle : float = 0
 
+var gyroscope_rates: Vector3 = Vector3.ZERO
 
-func _input(event):
-	pass
+
+func get_input(delta):
+	if Input.is_action_just_pressed("gyro_toggle"):
+		gyroscope_rates = Vector3.ZERO
+		use_gyroscope = not use_gyroscope
+	
+	if Input.is_action_just_pressed("cam_align_fwd"):
+		rotation = Vector3.ZERO
+	
+	if use_gyroscope:
+		rotate_x(gyroscope_rates.x * gyro_factor * delta)
+		rotate_y(gyroscope_rates.y * gyro_factor * delta)
+		rotate_z(gyroscope_rates.z * gyro_factor * delta)
+	else:
+		rotation.x = Input.get_axis("cam_down", "cam_up")
+		rotation.y = Input.get_axis("cam_left", "cam_right")
 
 
 func _ready() -> void:
 	$CardboardView.SetViewPorts($SubViewportL, $SubViewportR)
 
 
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	get_input(delta)
